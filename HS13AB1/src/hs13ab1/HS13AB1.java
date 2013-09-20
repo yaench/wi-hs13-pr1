093abc78b31a891b5c30bdc98e3ad4b861128ee5
yaench <>
2013-09-20 17:29:44 +0200

    Initial commit

---
+++ HS13AB1/src/hs13ab1/HS13AB1.java
@@ -0,0 +1,15 @@
+package hs13ab1;
+/**
+ *
+ * @author yaench
+ * @version
+ */
+public class HS13AB1 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ HS13AB1/src/hs13ab1/Summe.java
@@ -0,0 +1,15 @@
+package hs13ab1;
+/**
+ *
+ * @author yaench
+ * @version
+ * Erstelle ein Programm, welches alle Zahlen von 100 bis 200 summiert und 
+ * das Resultat auf der Console ausgibt.
+ */
+public class Summe {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
