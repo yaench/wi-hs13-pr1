b3eb815cf6d9c7a0707937f6fc6a97b796cf2e7c
yaench <>
2013-09-20 17:29:22 +0200

    Initial commit HelloWorld

---
+++ HS13AB1/src/vl01/helloworld/Vl01HelloWorld.java
@@ -0,0 +1,15 @@
+package vl01.helloworld;
+/**
+ *
+ * @author yaench
+ * @version
+ */
+public class Vl01HelloWorld {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
